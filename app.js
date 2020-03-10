712f9b005874e3f8f83da51b547c34956511fe6a
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-03-10 16:21:11 -0400

    Added controllers and routes for catalog. Functionality not yet implemented.

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'database url';
+var mongoDB = 'mongodb+srv://yoshiAdmin:tablet2020@cluster0-wmjxw.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
